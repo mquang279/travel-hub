7a772df6aafbac9d7744afe6b2ac87adc9ad403f
Mquang279 <quangm279@gmail.com>
2026-03-09 15:50:18 +0700

    first commit

---
+++ src/main/java/edu/uet/travel_hub/TravelHubApplication.java
@@ -0,0 +1,13 @@
+package edu.uet.travel_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelHubApplication.class, args);
+	}
+
+}
