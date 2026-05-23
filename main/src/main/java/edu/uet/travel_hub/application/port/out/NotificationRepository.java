526a60724944fdd96ec6ff872d3573e7aaea9190
mquang279 <quangm279@gmail.com>
2026-05-23 13:09:27 +0700

    fix: add target id to Notification entity

--- main/src/main/java/edu/uet/travel_hub/application/port/out/NotificationRepository.java
+++ main/src/main/java/edu/uet/travel_hub/application/port/out/NotificationRepository.java
@@ -1,10 +1,11 @@
 package edu.uet.travel_hub.application.port.out;
 
 import edu.uet.travel_hub.application.dto.response.PaginationResponse;
+import edu.uet.travel_hub.domain.enums.NotificationType;
 import edu.uet.travel_hub.domain.model.NotificationModel;
 
 public interface NotificationRepository {
-    void save(Long userId, String title, String body);
+    void save(Long userId, String title, String body, NotificationType type, Long targetId);
 
     PaginationResponse<NotificationModel> get(Long userId, int page, int pageSize);
 
